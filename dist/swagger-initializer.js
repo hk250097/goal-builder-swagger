d00522af1fdb0b774690b3e3ef4064590cb2fb06
hk250097 <hk250097@ncr.com>
2025-06-20 17:20:24 +0530

    Test Checkin

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -4,8 +4,8 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-      {url: "api/specs/gb-bff/GB-SVC.yaml", name: "GB BFF"},
-      {url: "api/specs/gb-svc/GB-BFF.yaml", name: "GB Service"},
+      {url: "api/specs/gb-bff/GB-BFF.yaml", name: "GB BFF"},
+      {url: "api/specs/gb-svc/GB-SVC.yaml", name: "GB Service"},
       {url: "api/specs/gb-notification-svc/GB-NOTIFICATION-SVC.yaml", name: "GB Notification Service"},
     ],
     dom_id: '#swagger-ui',
